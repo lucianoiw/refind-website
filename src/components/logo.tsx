cf327cf95e87262f5b473fe2a00ca3d9922a2079
Luciano Souza <lco.souza@gmail.com>
2025-11-16 15:08:31 -0300

    ajustes de favicon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,10 @@
-import { WaypointsIcon } from "lucide-react";
-
 import { APP_NAME, cn } from "@/utils";
+import { RefindLogo } from "./logos/refind";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("flex items-center space-x-2", className)}>
-      <WaypointsIcon className="size-6" />
+    <div className={cn("flex items-baseline space-x-2", className)}>
+      <RefindLogo className="size-6 fill-neutral-950 dark:fill-neutral-50" />
       <span className="text-nowrap text-2xl font-bold">{APP_NAME}</span>
     </div>
   );
